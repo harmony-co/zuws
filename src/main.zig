80eae6bced12b9db41908309c12c2f20bf3e19d6
DidaS <didasoficial@gmail.com>
2024-12-04 17:54:25 +0000

    Add all C bindings necessary (#2)

--- src/main.zig
+++ src/main.zig
@@ -7,6 +7,10 @@ pub fn main() !void {
     const app = c.uws_create_app();
     defer c.uws_app_destroy(app);
 
+    if (app == null) {
+        return error.CouldNotStartServer;
+    }
+
     c.uws_app_get(app, "/*", hello);
     c.uws_app_listen(app, 3000, null);
 
@@ -15,6 +19,6 @@ pub fn main() !void {
 
 fn hello(res: ?*c.uws_res_t, req: ?*c.uws_req_t) callconv(.C) void {
     _ = req;
-    const str = "Hello World!";
-    c.uws_res_end(res, str, str.len);
+    const str = "Hello World!\n";
+    c.uws_res_end(res, str, str.len, false);
 }

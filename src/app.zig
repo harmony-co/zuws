d66fd062192d78dac65b5041fd8b1cca70bfd5e5
Brian Ferri <me@brianferri.dev>
2024-12-13 12:47:05 +0100

    fix: pointer args

--- src/app.zig
+++ src/app.zig
@@ -8,7 +8,7 @@ pub const uWSError = error{
 const method_handler = *const fn (Response, Request) void;
 
 pub const Response = struct {
-    ptr: *c.uws_res_s,
+    ptr: ?*c.uws_res_s,
 
     pub fn close(res: *const Response) void {
         c.uws_res_close(res.ptr);
@@ -79,37 +79,37 @@ pub const Request = struct {
     ptr: ?*c.uws_req_s,
 
     pub fn isAncient(res: *const Request) bool {
-        return c.uws_req_is_ancient(res);
+        return c.uws_req_is_ancient(res.ptr);
     }
     pub fn getYield(res: *const Request) bool {
-        return c.uws_req_get_yield(res);
+        return c.uws_req_get_yield(res.ptr);
     }
     pub fn setYield(res: *const Request, yield: bool) void {
-        c.uws_req_set_yield(res, yield);
+        c.uws_req_set_yield(res.ptr, yield);
     }
     pub fn forEachHeader(res: *const Request, handler: c.uws_get_headers_server_handler) void {
-        c.uws_req_for_each_header(res, handler);
+        c.uws_req_for_each_header(res.ptr, handler);
     }
     pub fn getUrl(res: *const Request, dest: *[:0]const u8) usize {
-        return c.uws_req_get_url(res, dest);
+        return c.uws_req_get_url(res.ptr, dest);
     }
     pub fn getFullUrl(res: *const Request, dest: *[:0]const u8) usize {
-        return c.uws_req_get_full_url(res, dest);
+        return c.uws_req_get_full_url(res.ptr, dest);
     }
     pub fn getMethod(res: *const Request, dest: *[:0]const u8) usize {
-        return c.uws_req_get_method(res, dest);
+        return c.uws_req_get_method(res.ptr, dest);
     }
     pub fn getCaseSensitiveMethod(res: *const Request, dest: *[:0]const u8) usize {
-        return c.uws_req_get_case_sensitive_method(res, dest);
+        return c.uws_req_get_case_sensitive_method(res.ptr, dest);
     }
     pub fn getHeader(res: *const Request, lowerCaseHeader: [:0]const u8, lower_case_header_length: usize, dest: *[:0]const u8) usize {
-        return c.uws_req_get_header(res, lowerCaseHeader, lower_case_header_length, dest);
+        return c.uws_req_get_header(res.ptr, lowerCaseHeader, lower_case_header_length, dest);
     }
     pub fn getQuery(res: *const Request, key: [:0]const u8, key_length: usize, dest: *[:0]const u8) usize {
-        return c.uws_req_get_query(res, key, key_length, dest);
+        return c.uws_req_get_query(res.ptr, key, key_length, dest);
     }
     pub fn getParameter(res: *const Request, index: c_ushort, dest: *[:0]const u8) usize {
-        return c.uws_req_get_parameter(res, index, dest);
+        return c.uws_req_get_parameter(res.ptr, index, dest);
     }
 };
 

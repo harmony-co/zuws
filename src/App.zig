4c42357f8e21ed53bc84072a0acb5853c67224d3
Brian Ferri <me@brianferri.dev>
2025-02-02 21:16:49 +0100

    chore: update comment

--- src/App.zig
+++ src/App.zig
@@ -256,7 +256,7 @@ fn subscriptionWrapper(
 }
 
 /// **Args**:
-/// * `method` - A ***lowercase*** http method; refers to `bindings/uws.h:69:9`
+/// * `method` - A ***lowercase*** http method; refers to `bindings/uws.h:66:9`
 fn CreateMethodFn(comptime method: []const u8) fn (app: *const App, pattern: [:0]const u8, handler: MethodHandler) *const App {
     var temp_up: [8]u8 = undefined;
     const upper_method = std.ascii.upperString(&temp_up, method);

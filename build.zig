d1864da06af4d898bb4528acc7e4adaaf7e2c578
DidaS <didasoficial@gmail.com>
2025-06-19 07:26:48 +0100

    Allow disabling per-message deflate (-Dno_zlib)

--- build.zig
+++ build.zig
@@ -5,57 +5,13 @@ pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const config_options = b.addOptions();
     const debug_logs = b.option(bool, "debug_logs", "Whether to enable debug logs for route creation.") orelse (optimize == .Debug);
+    const no_zlib = b.option(bool, "no_zlib", "Wether to disable per-message deflate.") orelse false;
     const ssl = b.option(bool, "ssl", "Whether to enable SSL.") orelse false;
 
+    const config_options = b.addOptions();
     config_options.addOption(bool, "debug_logs", debug_logs);
     config_options.addOption(bool, "is_ssl", ssl);
-
-    const zlib_c = b.dependency("zlib", .{});
-    const zlib = b.addLibrary(.{
-        .name = "z",
-        .linkage = .static,
-        .root_module = b.createModule(.{
-            .target = target,
-            .optimize = optimize,
-        }),
-    });
-
-    zlib.linkLibC();
-    zlib.addCSourceFiles(.{
-        .root = zlib_c.path(""),
-        .files = &.{
-            "adler32.c",
-            "crc32.c",
-            "deflate.c",
-            "infback.c",
-            "inffast.c",
-            "inflate.c",
-            "inftrees.c",
-            "trees.c",
-            "zutil.c",
-            "compress.c",
-            "uncompr.c",
-            "gzclose.c",
-            "gzlib.c",
-            "gzread.c",
-            "gzwrite.c",
-        },
-        .flags = &.{
-            "-DHAVE_SYS_TYPES_H",
-            "-DHAVE_STDINT_H",
-            "-DHAVE_STDDEF_H",
-            "-DZ_HAVE_UNISTD_H",
-        },
-    });
-
-    zlib.installHeadersDirectory(zlib_c.path(""), "", .{
-        .include_extensions = &.{
-            "zconf.h",
-            "zlib.h",
-        },
-    });
 
     const us = b.dependency("uSockets", .{});
     const uSockets = b.addLibrary(.{
@@ -63,15 +19,66 @@ pub fn build(b: *std.Build) !void {
         .root_module = b.createModule(.{
             .target = target,
             .optimize = optimize,
+            .link_libc = true,
         }),
     });
 
-    uSockets.linkLibrary(zlib);
+    if (!no_zlib) {
+        const zlib_c = b.dependency("zlib", .{});
+        const zlib = b.addLibrary(.{
+            .name = "z",
+            .linkage = .static,
+            .root_module = b.createModule(.{
+                .target = target,
+                .optimize = optimize,
+                .link_libc = true,
+            }),
+        });
+
+        zlib.addCSourceFiles(.{
+            .root = zlib_c.path(""),
+            .files = &.{
+                "adler32.c",
+                "crc32.c",
+                "deflate.c",
+                "infback.c",
+                "inffast.c",
+                "inflate.c",
+                "inftrees.c",
+                "trees.c",
+                "zutil.c",
+                "compress.c",
+                "uncompr.c",
+                "gzclose.c",
+                "gzlib.c",
+                "gzread.c",
+                "gzwrite.c",
+            },
+            .flags = &.{
+                "-DHAVE_SYS_TYPES_H",
+                "-DHAVE_STDINT_H",
+                "-DHAVE_STDDEF_H",
+                "-DZ_HAVE_UNISTD_H",
+            },
+        });
+
+        zlib.installHeadersDirectory(zlib_c.path(""), "", .{
+            .include_extensions = &.{
+                "zconf.h",
+                "zlib.h",
+            },
+        });
+
+        uSockets.linkLibrary(zlib);
+    }
+
     uSockets.addIncludePath(us.path(""));
     uSockets.installHeader(us.path("libusockets.h"), "libusockets.h");
 
-    var uSocketsCFiles = std.ArrayList([]const u8).init(b.allocator);
-    try uSocketsCFiles.appendSlice(&.{
+    var uSockets_c_files = std.ArrayList([]const u8).init(b.allocator);
+    defer uSockets_c_files.deinit();
+
+    try uSockets_c_files.appendSlice(&.{
         "bsd.c",
         "context.c",
         "loop.c",
@@ -90,12 +97,12 @@ pub fn build(b: *std.Build) !void {
     if (ssl) {
         uSockets.linkLibCpp();
         try linkBoringSSL(b, uSockets);
-        try uSocketsCFiles.append("crypto/openssl.c");
+        try uSockets_c_files.append("crypto/openssl.c");
     }
 
     uSockets.addCSourceFiles(.{
         .root = us.path(""),
-        .files = try uSocketsCFiles.toOwnedSlice(),
+        .files = try uSockets_c_files.toOwnedSlice(),
         .flags = if (ssl) &.{"-DLIBUS_USE_OPENSSL"} else &.{"-DLIBUS_NO_SSL"},
     });
 
@@ -107,13 +114,19 @@ pub fn build(b: *std.Build) !void {
 
     uws.defineCMacro("ZUWS_USE_SSL", if (ssl) "1" else "0");
 
+    var uws_flags = try std.ArrayList([]const u8).initCapacity(b.allocator, 2);
+    defer uws_flags.deinit();
+
+    if (ssl) try uws_flags.append("-DZUWS_USE_SSL");
+    if (no_zlib) try uws_flags.append("-DUWS_NO_ZLIB");
+
     const uWebSockets = uws.addModule("uws");
     uWebSockets.link_libcpp = true;
     uWebSockets.linkLibrary(uSockets);
     uWebSockets.addCSourceFiles(.{
         .root = b.path("bindings/"),
         .files = &.{"uws.cpp"},
-        .flags = if (ssl) &.{"-DZUWS_USE_SSL"} else &.{},
+        .flags = try uws_flags.toOwnedSlice(),
     });
 
     const zuws = b.addModule("zuws", .{
